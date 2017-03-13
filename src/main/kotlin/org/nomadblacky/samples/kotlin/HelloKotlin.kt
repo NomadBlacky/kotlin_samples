8ef1f642b0da41799bccb43e92a0310208d1eb53
NomadBlacky <hourou.hamurinzu@gmail.com>
2017-03-13 18:10:04 +0900

    Hello Kotlin!

---
+++ src/main/kotlin/org/nomadblacky/samples/kotlin/HelloKotlin.kt
@@ -0,0 +1,10 @@
+package org.nomadblacky.samples.kotlin
+
+/**
+ * Created by blacky on 17/03/13.
+ */
+object HelloKotlin {
+    @JvmStatic fun main(args: Array<String>) {
+        println("Hello Kotlin!! 🐣")
+    }
+}
