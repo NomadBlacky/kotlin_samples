b4fe03eccd7f2cf9dc9397c79ad0e9e26bd7a2ac
NomadBlacky <hourou.hamurinzu@gmail.com>
2017-03-25 04:56:30 +0900

    Refactor Chapter02Spec.kt

--- src/test/kotlin/org/nomadblacky/samples/kotlin/nlp100/Chapter02Spec.kt
+++ src/test/kotlin/org/nomadblacky/samples/kotlin/nlp100/Chapter02Spec.kt
@@ -6,16 +6,24 @@ import io.kotlintest.specs.FunSpec
  * Created by blacky on 17/03/25.
  */
 class Chapter02Spec : FunSpec() {
+    private val resource = javaClass.getResource("hightemp.txt")!!
+
+    fun execProcess(vararg cmd: String): String {
+        val process = ProcessBuilder(*cmd).start()
+        process.waitFor()
+        val result = process.inputStream.bufferedReader().readText()
+        return result
+    }
+
     init {
         test("10. 行数のカウント") {
-            println(javaClass)
-            val rs = javaClass.getResource("hightemp.txt")
-            val lines = rs.readText().lines().filter(String::isNotEmpty).size
-            val pb = ProcessBuilder("wc", "-l", rs.path)
-            val processWc = pb.start()
-            processWc.waitFor()
-            val wcLines = processWc.inputStream.bufferedReader().readText().split(" ").first().toInt()
+            val lines = resource.readText().lines().filter(String::isNotEmpty).size
+            val wcLines = execProcess("wc", "-l", resource.path).split(" ").first().toInt()
             lines shouldBe wcLines
+        }
+
+        test("11. タブをスペースに置換") {
+            
         }
     }
 }
